e4113eac6689ca37c4f0923054442ff50febad3b
tirupdev <ulli.tirupataiah@gmail.com>
2025-08-08 11:30:54 +0530

    local-gen-nginx-reactapp2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import React from 'react';
+import packageJson from '../package.json';
+
 function App() {
   return (
-    <div style={{ textAlign: "center", marginTop: "50px", fontSize: "2rem" }}>
-      Tirup Ulli from Bangalore
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      Tirup Ulli from Bangalore<br />
+      Version: {packageJson.version}
     </div>
   );
 }
