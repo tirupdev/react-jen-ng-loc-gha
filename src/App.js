3a960a7826ec5c215383d053772b63e106da6f1e
tirupdev <ulli.tirupataiah@gmail.com>
2025-08-08 10:59:27 +0530

    local-gen-nginx-reactapp

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px", fontSize: "2rem" }}>
+      Tirup Ulli from Bangalore
+    </div>
+  );
+}
+
+export default App;
