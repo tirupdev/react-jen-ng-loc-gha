4e4915e287dbed8855f97eb899b0e01fd0a1839c
tirupdev <ulli.tirupataiah@gmail.com>
2025-08-12 13:04:00 +0530

    ghp

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import packageJson from '../package.json';
 function App() {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      Tirup Ulli from Bangalore v2 and from envi,from gha & 2workflows <br />
+      Tirup Ulli from Bangalore v2 and from envi,from gha & 4 workflows and gh pages and deploy in nginx as well(other pipelines) <br />
       Version: {packageJson.version}
     </div>
   );
